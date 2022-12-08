420a6bf2af69208830866d8cbdfdd1b90bc52a89
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-12-08 18:10:38 +0530

    added admin page

--- quizbot/subjects/views.py
+++ quizbot/subjects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Misc
 # Create your views here.
 def adminPage(request):
-    return render(request, 'subjects/admin.html')
+
+    data = Misc.objects.all()
+    return render(request, 'subjects/misc.html',{
+        'data':data,
+        'number' : len(data),
+    })
