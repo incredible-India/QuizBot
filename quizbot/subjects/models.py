c7d2e8ae668d80208f43dfb442f6ccd917a08c19
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-12-08 17:38:53 +0530

    added index and database for misc questions

--- quizbot/subjects/models.py
+++ quizbot/subjects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Misc(models.Model):
+    question = models.TextField(null=False,)
+    op1 = models.TextField(null=False,)
+    op2 = models.TextField(null=False,)
+    op3 = models.TextField(null=False,)
+    op4 = models.TextField(null=False,)
--- quizbot/subjects/views.py
+++ quizbot/subjects/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def adminPage(request):
+    return render(request, 'subjects/admin.html')
