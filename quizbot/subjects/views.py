617514a53a7c8b08f101f28cd3ebb816644004d1
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-12-09 17:48:33 +0530

    session work under control

--- quizbot/subjects/views.py
+++ quizbot/subjects/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from .models import Misc
 from django.views import View
+from django.contrib import messages
 # Create your views here.
 
 class adminPage(View):
 
     def get(self, request):
 
+
+        if 'question' in request.session:
+            question = request.session['question']
+        else:
+            question = ''
+
   
         data = Misc.objects.all()
         return render(request, 'subjects/misc.html',{
         'data':data,
         'number' : len(data),
+        'question' : question
         })
     def post(self, request):
         question = request.POST.get('question')
@@ -20,4 +28,19 @@ class adminPage(View):
         q3=request.POST.get('q3')
         q4=request.POST.get('q4')
 
+        #validation of question to be inserted 
+
+        if len(question.strip()) <= 5:
+             messages.info(request,'Question Cannot be blank or less than 5 characters..')
+
+             request.session['question'] = question
+
+             return HttpResponseRedirect('/settings')
+            
+        elif len(q1.strip())<=0 or len(q2.strip()) <=0 or len(q3.strip()) <= 0 or len(q4.strip()) <= 0:
+            messages.info(request,'Option cannot be blank')
+            request.session['question'] = question
+            return HttpResponseRedirect('/settings')
+        
         return HttpResponse(question)
+             
